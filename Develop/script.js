f0044f63361b8b4495437c0dc04f49c349ea99e6
Moetive <moetive206@gmail.com>
2022-05-05 23:37:31 -0700

    added for loop function for textbox styling

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,14 @@
     $("#time-display").text(moment().format('MMM DD, YYYY [at] hh:mm:ss a')); 
 
     // create function that defines past hours as grey
+    var textBox = $(".textBox")
+
+    for (var i = 0; i < textBox.length; i++) {
+        var textArea = $(textBox[i]);
+        let textBoxHour = textArea.attr("id");
+        let currentHour = moment().format('H');
+        if (currentHour > textBoxHour) {
+            textArea.addClass("past")
+        }
+    }
+ 
